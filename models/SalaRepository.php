1c0766faa40184ebb9d34687023b8fd49b533677
daulin <dpb28fpb@gmail.com>
2022-11-23 18:11:46 +0100

    hh

--- models/SalaRepository.php
+++
@@ -1,9 +0,0 @@
-<?php
-public static function getUserBymensaje($mensaje) {
-        $db = Conectar::conexion();
-        $result = $db -> query("SELECT * FROM mensajes WHERE  = '".$id_user."'");
-        if ($datos = $result -> fetch_assoc()) {
-            return new User($datos);
-        }
-    }
-?>
