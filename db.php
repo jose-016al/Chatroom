c633dff5a30312c6440f64f437a2df62bdd997a0
romen2232 <romen311299@gmail.com>
2022-11-25 19:17:28 +0100

    e

--- db.php
+++ db.php
@@ -1,9 +1,7 @@
 <?php
-class Conectar
-{
-    public static function conexion()
-    {
-        $conexion = new mysqli("localhost", "root", "", "chatRoom");
+class Conectar{
+    public static function conexion(){
+        $conexion=new mysqli("localhost", "root", "", "chatRoom");
         $conexion->query("SET NAMES 'utf8'");
         return $conexion;
     }
